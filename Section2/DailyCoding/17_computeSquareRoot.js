8f6627ad32b4afa9d045db9798fcb8d02d387014
sineTlsl <limsieun0313@gmail.com>
2023-02-07 09:43:45 +0900

    Daily Coding 17

--- Section2/DailyCoding/17_computeSquareRoot.js
+++ Section2/DailyCoding/17_computeSquareRoot.js
@@ -1,10 +1,20 @@
 let output = computeSquareRoot(6);
 console.log(output); // --> 2.45
 
+// let 근사값 = 원래 숫자 / 2;
+// while((근사값 ** 2) !== 원래 숫자) {
+//    근사값 = (근사값 + (원래 숫자 / 근사값)) / 2;
+// }
+
 function computeSquareRoot(num) {
-  let babylonian = 1;
-  for (let i = 0; i < 15; i++) {
-    babylonian = (babylonian + num / babylonian) / 2;
+  let approxNum = num / 2;
+
+  while (approxNum ** 2 !== num) {
+    if (Number((approxNum ** 2).toFixed(2)) === num) {
+      break;
+    }
+    approxNum = (approxNum + num / approxNum) / 2;
   }
-  return Number(babylonian.toFixed(2));
+
+  return Number(approxNum.toFixed(2));
 }
