de0ad024e2bc61358a8fa654fac9d4b8d80aeb3d
sineTlsl <limsieun0313@gmail.com>
2023-02-13 15:57:57 +0900

    Daily Coding 1

--- Section2/DailyCoding/06_letterCapitalize.js
+++
@@ -1,6 +0,0 @@
-let output1 = letterCapitalize('hello world');
-console.log(output1); // "Hello World"
-
-function letterCapitalize(str) {
-  return str.replace(/\b[a-z]/g, char => char.toUpperCase());
-}
