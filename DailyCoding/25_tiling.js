3c5f7fd99110b566c390163cdae4bd147008d618
sineTlsl <limsieun0313@gmail.com>
2023-02-25 17:21:03 +0900

    Daily Coding 25

---
+++ DailyCoding/25_tiling.js
@@ -0,0 +1,38 @@
+let tiling = function (n) {
+  const memo = [0, 1, 2];
+
+  const aux = (size) => {
+    if (memo[size] !== undefined) return memo[size];
+    if (size <= 2) return memo[n];
+    memo[size] = aux(size - 1) + aux(size - 2);
+    return memo[size];
+  };
+  return aux(n);
+};
+
+let output = tiling(4);
+console.log(output); // --> 5
+/* 
+2 x 4 보드에 타일을 놓는 방법은 5가지
+각 타일을 a, b, c, d로 구분
+
+2 | a b c d
+1 | a b c d 
+------------
+
+2 | a a c c
+1 | b b d d 
+------------
+
+2 | a b c c
+1 | a b d d 
+------------
+
+2 | a a c d
+1 | b b c d 
+------------
+
+2 | a b b d
+1 | a c c d 
+------------
+*/
