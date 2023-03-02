a967c2fc64a11659b541bd19b9726982724a2a83
sineTlsl <limsieun0313@gmail.com>
2023-03-02 18:07:02 +0900

    Daily Coding 26

---
+++ DailyCoding/26_treeDFS.js
@@ -0,0 +1,27 @@
+let dfs = function (node) {
+  let values = [node.value];
+
+  node.children.forEach((n) => {
+    values = values.concat(dfs(n));
+  });
+
+  return values;
+};
+
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
+
+let root = new Node(1);
+let rootChild1 = root.addChild(new Node(2));
+let rootChild2 = root.addChild(new Node(3));
+let leaf1 = rootChild1.addChild(new Node(4));
+let leaf2 = rootChild1.addChild(new Node(5));
+let output = dfs(root);
+console.log(output); // --> [1, 2, 4, 5, 3]
